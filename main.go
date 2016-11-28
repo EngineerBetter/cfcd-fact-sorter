4a4cb62aca52e72b151976802513d2952fab454a
Daniel Jones <daniel.jones@engineerbetter.com>
2016-11-28 11:11:51 +0000

    move types to separate file

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
-	"sort"
 )
 
 func main() {
@@ -45,49 +44,4 @@ func exists(path string) bool {
 	}
 
 	return true
-}
-
-type Fact struct {
-	Id          string
-	Description string
-}
-
-type ItemFacts struct {
-	ItemId string `yaml:"item_id"`
-	Facts  []Fact
-}
-
-func (itemFacts ItemFacts) Len() int {
-	return len(itemFacts.Facts)
-}
-
-func (itemFacts ItemFacts) Less(i, j int) bool {
-	return itemFacts.Facts[i].Id < itemFacts.Facts[j].Id
-}
-
-func (itemFacts ItemFacts) Swap(i, j int) {
-	itemFacts.Facts[i], itemFacts.Facts[j] = itemFacts.Facts[j], itemFacts.Facts[i]
-}
-
-type Items struct {
-	Items []ItemFacts
-}
-
-func (items Items) Sort() {
-	sort.Sort(items)
-	for _, item := range items.Items {
-		sort.Sort(item)
-	}
-}
-
-func (items Items) Len() int {
-	return len(items.Items)
-}
-
-func (items Items) Less(i, j int) bool {
-	return items.Items[i].ItemId < items.Items[j].ItemId
-}
-
-func (items Items) Swap(i, j int) {
-	items.Items[i], items.Items[j] = items.Items[j], items.Items[i]
 }
